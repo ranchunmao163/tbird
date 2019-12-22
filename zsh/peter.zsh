65f562777a3fb533a2e3e78a39574e92be76fa8e
Ran Peter <ranchunmao@gmail.com>
2019-12-23 04:25:57 +0800

    remove files and add certs folder

--- zsh/peter.zsh
+++ zsh/peter.zsh
@@ -1,1 +1,17 @@
 set -o vi
+
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_231.jdk/Contents/Home"
+export PATH="${JAVA_HOME}:${PATH}"
+
+export MAVEN_HOME="/Users/chran/bin/maven/latest"
+export PATH="${MAVEN_HOME}/bin:${PATH}"
+
+# fix makeinfo version issue
+export PATH="/usr/local/opt/texinfo/bin:$PATH"
+
+# fix autopoint and gettext command can't be found issue
+export PATH="/usr/local/opt/gettext/bin:$PATH"
+
+# Go
+export GOPATH="${HOME}/go"
+export PATH="${PATH}:${GOPATH}/bin"
